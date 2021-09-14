fe37696c52f95b9b95c52eeb719ea6a56690abf8
Juan AKA JRod <juan.rodriguez@qc-mfg.com>
2021-09-13 17:00:12 -0700

    working on the products page

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+function videoModal() {
+    var videoBox = document.getElementById("video-src");
+    //This is the Play Button Section
+    $(".video-btn-modal").click(() => {
+        $(".video-wrapper-modal").css("display", "flex");
+        videoBox.play();
+    });
+
+    //This is the Pause and Hide the Modal with the exit button
+    $(".video-exit-btn-modal").click(() => {
+        $(".video-wrapper-modal").css("display", "none");
+        videoBox.pause();
+    });
+
+    //This is the Pause and Hide the Modal with clicking outside the modal
+    $(".video-wrapper-modal").click(() => {
+        $(".video-wrapper-modal").css("display", "none");
+        videoBox.pause();
+    });
+}
+videoModal();
+
+
+
 // Mobile Nav Section
 function mobileNav() {
     $(".nav-mobile-btn").click(() => {
